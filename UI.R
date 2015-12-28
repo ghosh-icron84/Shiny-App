c889996b0a3aab59fadf095834b365e6eb130750
ghosh-icron84 <debapriya.usa@gmail.com>
2015-12-28 14:46:43 +0530

    Update UI.R

--- UI.R
+++ UI.R
@@ -1,42 +1,63 @@
 library(shiny)
 
 fluidPage (theme = "bootstrap.css", 
+        
   
-  titlePanel(h4(strong("TEXT ANALYTICS APP"),align = "center" )),
-  hr(),
+headerPanel(tags$head(h4(strong("Knowledge Search and Similarity Scoring System"),align = "center" )), tags$head(tags$img(src="image.png",align = "right", height = "85px"))),
+  br(),br(),
   
   sidebarLayout(
     
-    sidebarPanel(
-      textInput("selection", h5(strong("Upload text:")),
+    sidebarPanel( 
+      radioButtons("sel","Select action", selected ="file1",
+                   c("File Upload" = "file1","Insert Keywords" = "selection")),
+                     
+      
+       fileInput("file1", 'Upload File',accept=c('text/csv','text/comma-separated-values,text/plain')), 
+      
+      textInput("selection", h5(strong("Insert Keywords:")),
                   value = words),
-     actionButton("update1", "Submit", class="btn-info"),
+     actionButton("update1", "Submit",  class="btn btn-info"),
      
-      actionButton("update", "Parse", class="btn-info"),
-      hr(), hr(),
+      actionButton("update", "Generate",  class="btn btn-info"),
+    br(), br(),
+    dateInput("date1",label = "From", Sys.Date()),
+    dateInput("date2",label = "To", Sys.Date()),
+    br(),
+    
+    fluidRow( 
+            column(3,
       checkboxGroupInput("check", 
                          label = h5(strong("Word List")), 
-                         choices = list,
-                         selected = ""),
-      actionButton("update2", "Add",class="btn-info")
+                         choices = cho,
+                         selected = "")),
+      column(9,
+             actionButton("update2", "Add",  class="btn btn-info", align = "right")))),
+ 
+       
+    mainPanel(
       
-                ),
-    mainPanel( 
       tabsetPanel(id = 1,
-                  tabPanel(h5(strong("Input text")), textOutput("data1"))),
-                  hr(),
+                  tabPanel(h5(strong("Text Input")), tableOutput("data1"))
+                  ),
+      
+                  br(),
+      
                   #hr(),
             #hr(), hr(), hr(), 
-        tabsetPanel(id = 2,
-                  tabPanel(h5(strong("Output text")), textOutput("data"))),
-        actionButton("update3", "Add to Checkbox", class="btn-info"),
-           hr(), hr(),
+        #tabsetPanel(id = 2,
+                  #tabPanel(h5(strong("Output text")), textOutput("data"))),
+      
+        #actionButton("update3", "Add to Checkbox",  class="btn btn-info"),
+           br(), br(),
       tabsetPanel(id = 3,
                   tabPanel(h5(strong("Key words")), textOutput("data2"))),
-      downloadButton('downloadData', 'Download', class="btn-info")
+     # downloadButton('downloadData', 'Download', class="btn btn-info"),
+      actionButton("sear", "Search",  class="btn btn-info")
       )
     )
-  )
+)
+  
 
     
    
